21cfb7b3c6cb7ee24810f2dbdfdbdbd47278d44d
Artem Tsatsin <nuxdie@gmail.com>
2017-10-24 01:17:26 +0200

    🌎 use http to get remote image instead of local one 👩🏻 goodbye lena

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"os"
+	"io"
 	"fmt"
+	"net/url"
+	"net/http"
+
 	"github.com/ahmdrz/goinsta"
 	"github.com/ahmdrz/goinsta/response"
 )
@@ -11,16 +15,34 @@ func main() {
 
 	insta := login()
 
-	if len(os.Args) == 1 {
-		panic("Please provide a photo path.")
-	}
-
-	photoFile := os.Args[1]
+	photoFileReader := getPhoto()
 	photoCaption := "TODO #hashtags" // TODO: proper hash tags from Vision API
 
-	upload(insta, photoFile, photoCaption)
+	upload(insta, photoFileReader, photoCaption)
 
+	defer photoFileReader.Close()
 	defer insta.Logout()
+}
+
+func getPhoto() io.ReadCloser {
+	if len(os.Args) == 1 {
+		panic("Please provide a photo url.")
+	}
+
+	uri := os.Args[1]
+	parsed, err := url.Parse(uri)
+
+	if parsed.Host == "" || err != nil {
+		panic(fmt.Sprintf("Incorrect photo url provided: %s", uri))
+	}
+
+	resp, err := http.Get(uri)
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not get the photo by %s", uri))
+	}
+
+	return resp.Body
 }
 
 func login() * goinsta.Instagram {
@@ -41,12 +63,12 @@ func login() * goinsta.Instagram {
 	return insta
 }
 
-func upload(insta * goinsta.Instagram, path string, caption string) response.UploadPhotoResponse {
+func upload(insta * goinsta.Instagram, photo io.ReadCloser, caption string) response.UploadPhotoResponse {
 	quality := 87
 	uploadId := insta.NewUploadID()
 	filterType := goinsta.Filter_Normat
 
-	uploadPhotoResponse, err := insta.UploadPhoto(path, caption, uploadId, quality, filterType)
+	uploadPhotoResponse, err := insta.UploadPhotoFromReader(photo, caption, uploadId, quality, filterType)
 
 	if err != nil {
 		panic(err)
