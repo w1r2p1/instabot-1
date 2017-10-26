90bd57c519704069e0e3b10421b171188f8b00bd
Artem Tsatsin <nuxdie@gmail.com>
2017-10-26 23:48:29 +0200

    🔐 add google-vision-api-key.json 👁 add Google Vision API integration for #hashtags 📜 add setup and usage info to README

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 The aim of this project is to build a telegram bot, that accepts an image as a file or photo and uploads it to
 Instagram. It also adds appropriate tags and geotags based on image data. Video and gallery support could be added at
 later stages of the project.
+
+## Setup
+In order to get things working user needs to provide these environment variables:
+
+- Instagram credentials for an account to post to
+````bash
+INSTAGRAM_USERNAME=username
+INSTAGRAM_PASSWORD=passw0rd
+````
+
+- Google Vision API credentials for adding appropriate #hashtags,
+see [this guide to setup](https://cloud.google.com/docs/authentication/getting-started)
+````bash
+GOOGLE_APPLICATION_CREDENTIALS=/path/to/service/account/file.json
+````
+
+## Running
+The program expects a single parameter: photo file url. It could be run like so:
+````bash
+$ instabot https://bellard.org/bpg/lena30.jpg
+````
--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"os"
 	"io"
+	"io/ioutil"
 	"fmt"
+	"bytes"
 	"net/url"
 	"net/http"
+	"strings"
+	"context"
 
 	"github.com/ahmdrz/goinsta"
 	"github.com/ahmdrz/goinsta/response"
+
+	vision "cloud.google.com/go/vision/apiv1"
 )
 
 func main() {
 
 	insta := login()
 
-	photoFileReader := getPhoto()
-	photoCaption := "TODO #hashtags" // TODO: proper hash tags from Vision API
+	resp := getPhoto()
+	photoCaption := getHashtags(resp)
 
-	upload(insta, photoFileReader, photoCaption)
+	upload(insta, resp.Body, photoCaption)
 	/* TODO: turn off commenting
 	to do that i'll need to add media/configure route
 	[here](github.com/ahmdrz/goinsta/goinsta.go:841)
@@ -28,11 +34,51 @@ func main() {
 	for some inspiration could be found in js version
 	of [this api](https://github.com/huttarichard/instagram-private-api/blob/4f2c33dc177ac12ac555af4cfb9db15d98e882f2/client/v1/media.js#L215)
 	*/
-	defer photoFileReader.Close()
+	defer resp.Body.Close()
 	defer insta.Logout()
 }
 
-func getPhoto() io.ReadCloser {
+func getHashtags(resp * http.Response) string {
+	ctx := context.Background()
+	client, err := vision.NewImageAnnotatorClient(ctx)
+
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't start Google Vision Image Annotator Client: %s", err))
+	}
+
+	b := bytes.NewBuffer(make([]byte, 0)) // temporary buffer
+	photo := io.TeeReader(resp.Body, b) // returns a reader that writes contents of resp.Body to b
+
+	image, err := vision.NewImageFromReader(photo)
+
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't read photo: %s", err))
+	}
+
+	defer resp.Body.Close() // we're done w/ resp.Body
+	resp.Body = ioutil.NopCloser(b) // returns a ReadCloser w/ no-op Close
+
+	labels, err := client.DetectLabels(ctx, image, nil, 10)
+
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't detect image labels: %s", err))
+	}
+
+	defer ctx.Done()
+
+	res := ""
+
+	for _, label := range labels {
+		descr := label.Description
+		res += "#"
+		res += strings.Replace(descr, " ", "_", -1)
+		res += " "
+	}
+
+	return res
+}
+
+func getPhoto() * http.Response {
 	if len(os.Args) == 1 {
 		panic("Please provide a photo url.")
 	}
@@ -50,7 +96,7 @@ func getPhoto() io.ReadCloser {
 		panic(fmt.Sprintf("Could not get the photo by %s", uri))
 	}
 
-	return resp.Body
+	return resp
 }
 
 func login() * goinsta.Instagram {
@@ -79,7 +125,7 @@ func upload(insta * goinsta.Instagram, photo io.ReadCloser, caption string) resp
 	uploadPhotoResponse, err := insta.UploadPhotoFromReader(photo, caption, uploadId, quality, filterType)
 
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Couldn't upload photo to instagram: %s", err))
 	}
 
 	fmt.Printf("Upload status:%s MediaID: %s", uploadPhotoResponse.Status, uploadPhotoResponse.Media.ID)
